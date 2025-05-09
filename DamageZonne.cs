8165c27cb59602cf418d10f35a298d9f2ea50cdf
Osp07 <ingibjorg.osp.finns@gmail.com>
2025-05-09 12:17:28 +0000

    Update DamageZonne.cs

--- DamageZonne.cs
+++ DamageZonne.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class DamageZonne : MonoBehaviour
 {
     void OnTriggerStay2D(Collider2D other)
-   {
+    // ef að leikmaður kemur við damage zone
+    {
        PlayerController controller = other.GetComponent<PlayerController>();
+       // sækjum tilvísun í leikmanna skrifurna
 
        if (controller != null)
+       // ef að tilvísun í leikmann er sótt
        {
            controller.ChangeHealth(-1);
+           // leikmaður missir eitt stig, notum ChangeHealth() fallið
        }
-   }
+    }
 }
